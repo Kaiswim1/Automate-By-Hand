c65cd0b36f8a0967e55be789f62960287f8b0d77
Kaiswim1 <68935355+Kaiswim1@users.noreply.github.com>
2022-04-14 17:08:09 -0500

    Update bluetooth_led_with_phone.ino

--- bluetooth_led_with_phone.ino
+++ bluetooth_led_with_phone.ino
@@ -1,74 +1,46 @@
-#include <SoftwareSerial.h> 
-#include <SPI.h> 
-#include <SD.h>
+#include <SoftwareSerial.h>  
 
-SoftwareSerial BTSerial(6, 7); 
-Sd2Card card;
-SdVolume volume;
-SdFile root;  
-File myFile;
-int i;  
+SoftwareSerial BTSerial(0, 1);  
 
-void Read(String v)
-{
-  myFile = SD.open(v); 
-  if(myFile){
-    while (myFile.available()) { 
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-} 
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ENA, OUTPUT);  
+  pinMode(ENB, OUTPUT); 
+  pinMode(IN1, OUTPUT); 
+  pinMode(IN2, OUTPUT);  
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT);    
 
-void Write(String v, int i)
-{
-  myFile = SD.open(v, FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  Serial.begin(9600);
+  BTSerial.begin(9600);   
+  Serial.println(BTSerial.available()); 
 }
 
-void setup()
-{   
-   i=0;
-   Serial.begin(9600); 
-   BTSerial.begin(9600);
-   pinMode(13, OUTPUT); // put your setup code here, to run once: 
-   while (!Serial) {;} // wait for serial port to connect. Needed for native USB port only   
-   Serial.print("Initializing SD card...");
-   Serial.println("initialization done.");
-}
-
-
-void loop()
-{
-   // put your main code here, to run repeatedly:
-
-   if (BTSerial.available() > 0)
-   {
-      char data = BTSerial.read(); // reading the data received from the bluetooth module
-      switch (data)
-      { 
-         case 'v': myFile = SD.open("internal.txt", FILE_WRITE); Write("internal.txt", 0); break;  
-         case 'x': myFile = SD.open("internal.txt"); Serial.print(myFile.read()); break;
-         case 'a': digitalWrite(13, HIGH); myFile.println("a\n"+String(i)); break; // when a is pressed on the app on your smart phone
-         case 'd': digitalWrite(13, LOW); myFile.println("d\n"+String(i)); break; // when d is pressed on the app on your smart phone
-         default : break;
-      }
+void loop() {   
+  if (BTSerial.available())
+  { 
+    Serial.println("Available"); 
+    char data = BTSerial.read(); // reading the data received from the bluetooth module 
+    Serial.println(data); 
+    if(data == 'f'){ //Forward
+      digitalWrite(IN1, HIGH);  
+      digitalWrite(IN2, LOW);  
+      digitalWrite(IN3, HIGH);  
+      digitalWrite(IN4, LOW); 
+      analogWrite(ENA, 185); 
+      analogWrite(ENB, 185);     
+    } 
+    if(data == 's'){ //Stop
+      analogWrite(ENA, 0); 
+      analogWrite(ENB, 0); 
    }
-   delay(1); 
-   i++;
 }
+}
+
+         
