9237723f67592629463498714167a598d9c8a166
Kaiswim1 <68935355+Kaiswim1@users.noreply.github.com>
2022-05-03 22:47:37 -0500

    Update bluetooth_led_with_phone.ino

--- bluetooth_led_with_phone.ino
+++ bluetooth_led_with_phone.ino
@@ -22,44 +22,70 @@ void setup() {
   Serial.println(BTSerial.available()); 
 }
 
-void loop() {   
+void loop() {     
+  int speed = 155;
   if (BTSerial.available())
   { 
     Serial.println("Available"); 
     char data = BTSerial.read(); // reading the data received from the bluetooth module 
     Serial.println(data); 
-    if(data == 'r'){ //right
+    if(data == 'r'){ //right  
       digitalWrite(IN1, HIGH);  
       digitalWrite(IN2, LOW);  
       digitalWrite(IN3, HIGH);  
       digitalWrite(IN4, LOW); 
-      analogWrite(ENA, 185); 
-      analogWrite(ENB, 185);     
+      analogWrite(ENA, speed); 
+      analogWrite(ENB, speed);     
     }  
-    if(data == 'l'){
+    if(data == 'l'){  //left
       digitalWrite(IN1, LOW);  
       digitalWrite(IN2, HIGH);  
       digitalWrite(IN3, LOW);  
       digitalWrite(IN4, HIGH); 
-      analogWrite(ENA, 185); 
-      analogWrite(ENB, 185);   
+      analogWrite(ENA, speed); 
+      analogWrite(ENB, speed);   
     } 
-    if(data == 'u'){
+    if(data == 'u'){  //up 
       digitalWrite(IN1, HIGH);  
       digitalWrite(IN2, LOW);  
       digitalWrite(IN3, LOW);  
       digitalWrite(IN4, HIGH); 
-      analogWrite(ENA, 185); 
-      analogWrite(ENB, 185);   
+      analogWrite(ENA, speed); 
+      analogWrite(ENB, speed);   
     } 
-     if(data == 'd'){
+     if(data == 'd'){  //down 
       digitalWrite(IN1, LOW);  
       digitalWrite(IN2, HIGH);  
       digitalWrite(IN3, HIGH);  
       digitalWrite(IN4, LOW); 
-      analogWrite(ENA, 185); 
-      analogWrite(ENB, 185);   
-    }
+      analogWrite(ENA, speed); 
+      analogWrite(ENB, speed);   
+    } 
+    if(data == 'w'){ //right 90 
+      digitalWrite(IN1, HIGH);  
+      digitalWrite(IN2, LOW);  
+      digitalWrite(IN3, HIGH);  
+      digitalWrite(IN4, LOW);  
+      analogWrite(ENA, 255); 
+      analogWrite(ENB, 255);    
+      delay(311);
+      analogWrite(ENA, 0); 
+      analogWrite(ENB, 0);  
+    }   
+     if(data == 'q'){ //right 90
+      analogWrite(ENA, 0); 
+      analogWrite(ENB, 0);  
+      delay(40); 
+      digitalWrite(IN1, LOW);  
+      digitalWrite(IN2, HIGH);  
+      digitalWrite(IN3, LOW);  
+      digitalWrite(IN4, HIGH);   
+      analogWrite(ENA, 255); 
+      analogWrite(ENB, 255);    
+      delay(311);
+      analogWrite(ENA, 0); 
+      analogWrite(ENB, 0);  
+    }  
     if(data == 's'){ //Stop
       analogWrite(ENA, 0); 
       analogWrite(ENB, 0); 
