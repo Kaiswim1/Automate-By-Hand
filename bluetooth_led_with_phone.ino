f087de8a5f950e63264b51f081b4cf9c8400988e
Kaiswim1 <68935355+Kaiswim1@users.noreply.github.com>
2022-06-25 20:14:41 -0500

    Update bluetooth_led_with_phone.ino

--- bluetooth_led_with_phone.ino
+++ bluetooth_led_with_phone.ino
@@ -41,7 +41,16 @@ void setup() {
   Wire.write(0x6B); // PWR_MGMT_1 register
   Wire.write(0); // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
+} 
+
+void correctLeft(int duration){
+  
+} 
+
+void correctRight(int duration){
+  
 }
+
 void loop() { 
 
   int speed = 155;
